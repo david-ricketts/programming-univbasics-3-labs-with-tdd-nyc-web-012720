a5ba51ce3ad46eea1da6f358f34f02236bd40d58
David Ricketts <david.ricketts119@gmail.com>
2020-01-06 18:07:46 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,11 @@
 # Add your variables here
 
-first_number = "Hello World"
+first_number = 1.1
+second_number = 2 
+
+sum =  first_number + second_number
+difference = first_number - second_number
+product =  first_number * second_number
+quotient = first_number /
 
 
